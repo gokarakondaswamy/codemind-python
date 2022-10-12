e32215065ea91fd91bc3f547f85d331bde7f01a2
gokarakondaswamy <106660146+gokarakondaswamy@users.noreply.github.com>
2022-10-12 14:53:52 +0530

    def count(n):     n=list(n)     v='aeiouAEIOU'     c=0     i=0     j=len(n)-1     for i in range(len(n)//2):         if (n[i]in v and n[j] not in v) or (n[i]not in v and n[j]in v):             c+=1         j-=1     return c a=input().split() c=0 for i in a:     c+=count(i) print(c)

---
+++ count_vowel_consonent_pairs_in_words.py
@@ -0,0 +1,16 @@
+def count(n):
+    n=list(n)
+    v='aeiouAEIOU'
+    c=0
+    i=0
+    j=len(n)-1
+    for i in range(len(n)//2):
+        if (n[i]in v and n[j] not in v) or (n[i]not in v and n[j]in v):
+            c+=1
+        j-=1
+    return c
+a=input().split()
+c=0
+for i in a:
+    c+=count(i)
+print(c)
